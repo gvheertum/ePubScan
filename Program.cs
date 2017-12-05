c977e9749ea40eaa3041d257ea81d1bbb30e5ac0
Gertjan van Heertum <git@netietsmeer.nl>
2017-12-05 11:47:13 +0100

    First commit, reading zips yo

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace EpubAnalyzer
+{
+    class Program
+    {
+		private const string TestEpubFile = "/Users/gertjan/Desktop/test.epub";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+			new Program().TestZip(TestEpubFile);
+        }
+
+		private void TestZip(string file)
+		{
+			if(!System.IO.File.Exists(file)) { throw new Exception("Invalid file"); }
+			using(var za = ZipFile.Open(file, ZipArchiveMode.Read))
+			{
+				foreach(var x in za.Entries)
+				{
+					System.Console.WriteLine($":{x.FullName} ({x.Length})");
+				}
+			}
+		}
+    }
+}
