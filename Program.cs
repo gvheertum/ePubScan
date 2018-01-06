cf9ce9151f661e312bc469f696fdbbbe62ec3e58
Gertjan van Heertum <git@netietsmeer.nl>
2018-01-06 17:39:22 +0100

    Writing an html file with the content to disk

--- Program.cs
+++ Program.cs
@@ -5,12 +5,15 @@ using System.Collections.Generic;
 using System.Xml.Linq;
 using System.IO;
 using EpubAnalyzer.FileParsing;
+using System.Text;
+using EpubAnalyzer.Entities;
 
 namespace EpubAnalyzer
 {
     class Program
     {
 		private const string TestEpubFolder = "/Users/gertjan/Desktop/epubtest/";
+		private const string TestEpubOutput = "/Users/gertjan/Desktop/epub-output/";
         static void Main(string[] args)
         {
 
@@ -20,9 +23,10 @@ namespace EpubAnalyzer
 			Console.WriteLine("Default path: " + TestEpubFolder);
 			Console.WriteLine("Folder (empty to keep default): ");
 			var folderToUse = Console.ReadLine();
-			folderToUse = !string.IsNullOrWhiteSpace(folderToUse) ? TestEpubFolder : folderToUse;
+			System.Console.WriteLine($"User input: {folderToUse}");
+			folderToUse = !string.IsNullOrWhiteSpace(folderToUse) ? folderToUse : TestEpubFolder;
 
-			var files = new FolderScanner().GetEpubFilesFromFolder(TestEpubFolder);
+			var files = new FolderScanner().GetEpubFilesFromFolder(folderToUse);
 			System.Console.WriteLine($"Retrieved {files.Count} files");
 			
 			
@@ -40,6 +44,68 @@ namespace EpubAnalyzer
 				System.Console.WriteLine($"Date: {data.Date}");
 				System.Console.WriteLine($"ISBN: {data.ISBN}");
 			});
+
+			Console.WriteLine("Default output path: " + TestEpubOutput);
+			Console.WriteLine("Folder (empty to keep default): ");
+			var folderToUseForOutput = Console.ReadLine();
+			folderToUseForOutput = !string.IsNullOrWhiteSpace(folderToUseForOutput) ? folderToUseForOutput : TestEpubOutput;
+			WriteOutputFile(epubDetails, folderToUseForOutput);
         }
+//TODO: isolate writing logic
+		private static void WriteOutputFile(IEnumerable<EbookData> data, string folder)
+		{
+			if(!System.IO.Directory.Exists(folder)) { System.IO.Directory.CreateDirectory(folder); }
+			var di = new System.IO.DirectoryInfo(folder);
+			string outputFile = di.FullName + "/" + DateTime.Now.ToString("yyyy-MM-dd") + ".html";
+			System.Console.WriteLine($"Writing content to: {outputFile}");
+			System.IO.File.WriteAllText(outputFile, GetTableContent(data));
+		}
+
+		private static string GetTableContent(IEnumerable<EbookData> data) 
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append("<html><body>");
+			sb.Append("<table>");
+			sb.Append(GenerateHeader());
+			data.ToList().ForEach(d => sb.Append(GenerateLine(d)));
+			sb.Append("</table>");
+			sb.Append("</body></html>");
+			return sb.ToString();
+		}
+
+
+		private static string GenerateHeader()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append($"<tr>");
+			sb.Append($"<th>ISBN</th>");
+			sb.Append($"<th>Title</th>");
+			sb.Append($"<th>Author</th>");
+			sb.Append($"<th>Language</th>");
+			sb.Append($"<th>Subject</th>");
+			sb.Append($"<th>Description</th>");
+			sb.Append($"<th>Date</th>");
+			sb.Append($"<th>Folder</th>");
+			sb.Append($"<th>FileName</th>");
+			sb.Append($"</tr>");
+			return sb.ToString();
+		}
+
+		private static string GenerateLine(EbookData data) 
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append($"<tr>");
+			sb.Append($"<td>{data.ISBN}</td>");
+			sb.Append($"<td>{data.Title}</td>");
+			sb.Append($"<td>{data.Author}</td>");
+			sb.Append($"<td>{data.Language}</td>");
+			sb.Append($"<td>{data.Subject}</td>");
+			sb.Append($"<td>{data.Description}</td>");
+			sb.Append($"<td>{data.Date}</td>");
+			sb.Append($"<td>{data.Folder}</td>");
+			sb.Append($"<td>{data.FileName}</td>");
+			sb.Append($"</tr>");
+			return sb.ToString();
+		}
     }	
 }
