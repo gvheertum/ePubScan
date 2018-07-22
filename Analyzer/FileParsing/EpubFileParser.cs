c6766fbbce036593ec95c00c0f9780bb7bd19394
Gertjan van Heertum <git@netietsmeer.nl>
2018-07-22 12:29:47 +0200

    Populated new fields of the books in the parser

--- Analyzer/FileParsing/EpubFileParser.cs
+++ Analyzer/FileParsing/EpubFileParser.cs
@@ -12,6 +12,12 @@ namespace EpubAnalyzer.FileParsing
 		{
 			FileInfo fi = new FileInfo(file);
 			EbookData data = new EpubOpfReader().GetData(fi) ?? new EpubNcxReader().GetData(fi);
+			if(data!=null)
+			{
+				data.BookDetail.Medium = "ebook";
+				data.BookDetail.Status = "N/A";
+				data.BookDetail.StatusRemark = "Not applicable, is ebook";
+			}
 			return data ?? throw new Exception($"Cannot read data from OPF and NCX for {fi.Name}");;
 		}
 
