6ac3dca628bf5768fac3e3e755cad376e30752f1
Gertjan van Heertum <git@netietsmeer.nl>
2018-09-16 11:05:49 +0200

    Binding from server request now works (check paths in future), copy constructor to KO binding implemented, added async table for further implementation on the book index screen.

--- Catalog/wwwroot/js/book.controller.js
+++ Catalog/wwwroot/js/book.controller.js
@@ -1,14 +1,49 @@
-var BookCollection = /** @class */ (function () {
-    function BookCollection() {
+var BookCollectionViewModel = /** @class */ (function () {
+    function BookCollectionViewModel() {
+        this.Collection = ko.observable([]);
+        this.loadFromServer();
     }
-    BookCollection.prototype.LoadFromServer = function () {
+    BookCollectionViewModel.prototype.loadFromServer = function () {
+        var _self = this;
         $.getJSON("/api/books/all/", function (data) {
-            // Now use this data to update your view models, 
-            // and Knockout will update your UI automatically 
-			console.log(data.length);
-			console.log(data);
+            //TODO: Check if and how we can use the .mapping from KO here. 
+            //Example online states a mapping on the whole viewmodel, while we only want to do a part
+            var bookArray = [];
+            for (var i = 0; i < data.length; i++) {
+                bookArray.push(new Book(data[i]));
+            }
+            _self.Collection(bookArray);
         });
     };
-    return BookCollection;
+    return BookCollectionViewModel;
+}());
+ko.applyBindings(new BookCollectionViewModel());
+var Book = /** @class */ (function () {
+    function Book(input) {
+        this.author = ko.observable("");
+        this.bookID = ko.observable(0);
+        this.category = ko.observable("");
+        this.description = ko.observable("");
+        this.fileName = ko.observable("");
+        this.folder = ko.observable("");
+        this.identifier = ko.observable("");
+        this.language = ko.observable("");
+        this.medium = ko.observable("");
+        this.readRemark = ko.observable("");
+        this.readStatus = ko.observable("");
+        this.status = ko.observable("");
+        this.statusRemark = ko.observable("");
+        this.subject = ko.observable("");
+        this.title = ko.observable("");
+        if (input != null) {
+            //Look for all properties in the input and map them to our own property functions
+            for (var property in input) {
+                if (this[property] != null) {
+                    this[property](input[property]);
+                }
+            }
+        }
+    }
+    return Book;
 }());
 //# sourceMappingURL=book.controller.js.map
--- Catalog/wwwroot/js/book.controller.ts
+++ Catalog/wwwroot/js/book.controller.ts
@@ -1,18 +1,60 @@
-declare var ko : any;
-declare var $ : any;
-
-class BookCollection
+class BookCollectionViewModel
 {
-	public LoadFromServer() : void
+	public Collection : KnockoutObservable<Array<Book>> = ko.observable([]);
+	constructor()
 	{
+		this.loadFromServer();
+	}
+	
+	public loadFromServer() : void
+	{
+		var _self = this;
 		$.getJSON("/api/books/all/", function(data : Array<IBook>) { 
-			// Now use this data to update your view models, 
-			// and Knockout will update your UI automatically 
-			console.log(data);
-		})
+			//TODO: Check if and how we can use the .mapping from KO here. 
+			//Example online states a mapping on the whole viewmodel, while we only want to do a part
+			var bookArray : Array<Book> = [];
+			for(var i = 0; i < data.length; i++)
+			{
+				bookArray.push(new Book(data[i]));
+			}
+			_self.Collection(bookArray);
+		});
 	}
 }
 
+ko.applyBindings(new BookCollectionViewModel());
+class Book
+{
+	public constructor(input?: IBook)
+	{
+		if(input != null)
+		{
+			//Look for all properties in the input and map them to our own property functions
+			for (var property in input) 
+			{
+				if(this[property] != null)
+				{
+					this[property](input[property]);
+				}
+			}
+		}
+	}
+	public author: KnockoutObservable<string> = ko.observable("");
+	public bookID: KnockoutObservable<number> = ko.observable(0);
+	public category: KnockoutObservable<string> = ko.observable("");
+	public description: KnockoutObservable<string> = ko.observable("");
+	public fileName: KnockoutObservable<string> = ko.observable("");
+	public folder: KnockoutObservable<string> = ko.observable("");
+	public identifier: KnockoutObservable<string> = ko.observable("");
+	public language: KnockoutObservable<string> = ko.observable("");
+	public medium: KnockoutObservable<string> = ko.observable("");
+	public readRemark: KnockoutObservable<string> = ko.observable("");
+	public readStatus: KnockoutObservable<string> = ko.observable("");
+	public status: KnockoutObservable<string> = ko.observable("");
+	public statusRemark: KnockoutObservable<string> = ko.observable("");
+	public subject: KnockoutObservable<string> = ko.observable("");
+	public title: KnockoutObservable<string> = ko.observable("");
+}
 interface IBook
 {
 	author: string;
