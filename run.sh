8096d4482ce40e6be4e3c3b93635a700add128f4
Gertjan van Heertum <git@heertum.net>
2021-03-29 21:22:48 +0200

    Added docker playground

---
+++ run.sh
@@ -0,0 +1,6 @@
+dotnet publish Catalog.KO/ -c Release -o out 
+
+docker build -t catalogapi Catalog.API/.
+docker build -t catalogko Catalog.KO/.
+docker run -d -p 1234:80 --name CatalogKO catalogko
+docker run -d -p 4321:80 --name CatalogAPI catalogapi
