b6b6f8c8c067d7db52f49d49b6a64da4cd7382a4
Gertjan van Heertum <git@netietsmeer.nl>
2023-11-12 17:01:20 +0100

    detail link

--- Catalog.Next/catalog/components/bookline.tsx
+++ Catalog.Next/catalog/components/bookline.tsx
@@ -35,33 +35,6 @@ export default function BookLine({
       <ReadStatusBadge readStatus={book?.ReadStatus} nrOfPages={book?.NrOfPages} />
     
     </TableCell>
-    <TableCell align="right">ACTIES</TableCell>
+    <TableCell align="right"><Link href={"book/" + book?.BookID}>Details</Link></TableCell>
   </TableRow>
-
-  //   <Accordion TransitionProps={{ unmountOnExit: true }} >
-  //     <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-  //     <Badge badgeContent={book?.NrOfPages ?? "?"} color="primary"><Typography>{book?.Author} - {book?.Title}</Typography></Badge>
-  //   </AccordionSummary>
-  // <AccordionDetails>
-  //   {book?.Author} - {book?.Title}
-  //             {book?.Description}
-  //           <Button size="small" color="primary" variant='contained' href={"/book/" + book?.BookID}>View Details</Button>
-
-  //         <div>
-  //           <button onClick={async () => await updateReadTag("")}>-</button>
-  //           <button onClick={async () => await updateReadTag("Read")}>Read</button>
-  //           <button onClick={async () => await updateReadTag("Reading")} >Reading</button>
-  //           <button onClick={async () => await updateReadTag("To Read")} >Te Read</button>
-  //           <button onClick={async () => await updateReadTag("Not going to read")} >Not going to read</button>
-
-  //       </div>
-  //   </AccordionDetails>
-  //   </Accordion>
-
-
-
-
-
-
-
 }
