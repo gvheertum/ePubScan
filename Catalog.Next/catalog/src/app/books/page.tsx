aa8a97503412eb8d883823676de8067a92620c1a
Gertjan van Heertum <git@netietsmeer.nl>
2023-11-13 22:10:56 +0100

    lookup and search

--- Catalog.Next/catalog/src/app/books/page.tsx
+++ Catalog.Next/catalog/src/app/books/page.tsx
@@ -1,14 +1,18 @@
 'use client'
-import { Table, TableCell, TableContainer, TableHead, TableRow, TableBody, Paper, Backdrop, CircularProgress } from "@mui/material";
+import { Table, TableCell, TableContainer, TableHead, TableRow, TableBody, Paper, Backdrop, CircularProgress, Chip } from "@mui/material";
 import BookLine from "../../../components/bookline";
-import { IBook } from "../../../lib/IBook";
+import { IBook, ReadStateElement, ReadStates } from "../../../lib/IBook";
 import BookRepository from "../../../lib/bookrepository"
 import { useEffect, useState } from "react";
 import ApiConsumer from "../../../lib/apiconsumer";
 
 export default function BookOverview() {
+    const [allbooks, setAllBooks]: [IBook[], any] = useState([]);
     const [books, setBooks]: [IBook[], any] = useState([]);
     const [loading, setLoading]: [boolean, any] = useState(true);
+    const [searchstring, setSearchString] = useState("");
+
+    
 
 //TODO: Add filter logic
 //TODO: Add search
@@ -16,17 +20,60 @@ export default function BookOverview() {
 
 
     const getData = () => {
+        console.log("Getting data!");
         setLoading(true);
         new ApiConsumer().getAllBooks().then(d => {
-            console.log("Data:", d);
-            setBooks(d?.reverse());
+            var bookData = d?.reverse() ?? [];
+            setAllBooks(bookData);
+            setBooks(bookData);
             setLoading(false);
         })
+    }
+
+    const filterName = (search: string) => {
+        setLoading(true);
+        console.log("filtering on:", search);
+        let filteredBooks = allbooks;
+        if(search != "") {
+            filteredBooks = allbooks.filter((b,i) => { 
+                return (b.Author && b.Author!.toLocaleLowerCase().indexOf(search.toLocaleLowerCase()) > -1) || 
+                    (b.Title && b.Title!.toLocaleLowerCase().indexOf(search.toLocaleLowerCase()) > -1) 
+            });
+        } 
+        
+        setBooks(filteredBooks);
+        setLoading(false);
+    }
+
+    const resetFilter = () => {
+        setLoading(true);
+        setBooks(allbooks);
+        setLoading(false);
+    }
+
+    const filterStatus = (status: ReadStateElement) => {
+        setLoading(true);
+        
+        let filteredBooks = allbooks.filter((b,i) => { 
+            return b.ReadStatus == status.code || b.ReadStatus == status.display 
+        });
+        
+        setBooks(filteredBooks);
+        setLoading(false);
     }
 
     useEffect(() => { getData() }, [])
 
     return <>
+        <Chip variant="outlined" onClick={() => resetFilter()} label="No filter" />
+        <Chip onClick={() => filterStatus(new ReadStates().Read)} color="success" label="Read" />
+        <Chip onClick={() => filterStatus(new ReadStates().Reading)} color="primary" label="Reading" />
+        <Chip onClick={() => filterStatus(new ReadStates().ToRead)} color="success" label="To Read" />
+        <Chip onClick={() => filterStatus(new ReadStates().WontRead)} color="error" label="Wont read" />
+        <Chip onClick={() => filterStatus(new ReadStates().Unknown)} color="default" label="Unknown" /> <br/>
+        <input type="text" placeholder="Search text" id="txtSearch" onBlur={(v)=> setSearchString(v.target.value) } />
+        <button onClick={() => filterName(searchstring) }>Search</button>
+
         {!loading &&
             <TableContainer component={Paper} sx={{ width: '100%', overflow: 'hidden' }}>
                 <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
