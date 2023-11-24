4e5796fbbfce408bb75be75ccddb76bb6e9f3ed1
Gertjan van Heertum <git@netietsmeer.nl>
2023-11-24 20:36:50 +0100

    Working commit

--- Catalog.Next/catalog/lib/userrepository.ts
+++ Catalog.Next/catalog/lib/userrepository.ts
@@ -1,3 +1,4 @@
+import { constants } from 'fs';
 import prisma from './prisma';
 import bcrypt from 'bcrypt';
 
@@ -10,11 +11,11 @@ export default class UserRepository {
 
   async getUser(email : string, password: string): Promise<ICatalogUser| null> {
     console.log("user:", email, "password", password); 
-    let user: ICatalogUser | null = await prisma.catalogUser.findFirst({
+    const user: ICatalogUser | null = await prisma.catalogUser.findFirst({
       where: {
         Email: email,
       }
-    });
+    }) as ICatalogUser | null;
     
     console.log("Found user:", user);
 
@@ -24,7 +25,7 @@ export default class UserRepository {
       // let genPw = await bcrypt.hash(password, 10);
       // console.log("tried pw:", password, "h->", genPw);
       
-      pwComp = await bcrypt.compare(password, user?.PasswordHash);  
+      pwComp = await bcrypt.compare(password, user?.PasswordHash ?? "");  
     }
     catch(ex) {}
 
