20b763abd75f8bd88a13d97cc035ed28f692657f
Gertjan van Heertum <git@netietsmeer.nl>
2023-11-24 21:30:11 +0100

    Changed login

--- Catalog.Next/catalog/src/app/login/page.tsx
+++ Catalog.Next/catalog/src/app/login/page.tsx
@@ -1,16 +1,53 @@
-import LoginForm from "../../../components/login-form";
+'use client';
+
+import { Avatar, Button, Checkbox, CssBaseline, FormControl, FormControlLabel, Input, InputLabel, Paper, Typography, withStyles } from "@mui/material";
+import { useFormState, useFormStatus } from 'react-dom';
+import { authenticate } from "../../../lib/actions";
+import "./login.css"
 
 export default function LoginPage() {
+  const [state, dispatch] = useFormState(authenticate, undefined);
+
   console.log("loading login!");
     return (
-      <main className="flex items-center justify-center md:h-screen">
-        <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-          <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-            <div className="w-32 text-white md:w-36">
-            </div>
-          </div>
-          <LoginForm />
+      <main>
+    
+      <Paper className="paper-login" >
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form action={dispatch}>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="email">Email Address</InputLabel>
+            <Input id="email" name="email" autoComplete="email" autoFocus />
+          </FormControl>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input name="password" type="password" id="password" autoComplete="current-password" />
+          </FormControl>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+          >
+            Sign in
+          </Button>
+        </form>
+      </Paper>
+
+      <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+          {state === 'CredentialsSignin' && (
+            <>
+              {/* <ExclamationCircleIcon className="h-5 w-5 text-red-500" /> */}
+              <p className="text-sm text-red-500">Invalid credentials</p>
+            </>
+          )}
         </div>
-      </main>
+    </main>
     );
   }
